a82843ba90fa2a6b7fb742a9fa1e8e007007d6b3
s1r-J <s1r.aria.azureglo@gmail.com>
2019-09-14 12:58:48 +0900

    Init

---
+++ README.md
@@ -0,0 +1,42 @@
+node-red-contrib-flow-asserter
+====
+
+Node-RED nodes to help testing your flow.
+
+## Description
+
+This module has 2 nodes, **flow-asserter in node** and **flow-asserter out node**.
+
+![flow-asserter](./images/assertflow.png)  
+![testcases](./images/testcases.png)
+
+Testcases are written in flow-asserter in node and flow-asserter out node is inserted into flow at the point wanted to assert <code>msg.payload</code>. By clicking the button, test will start. Each testcase stores its input in <code>msg.payload</code> and sends a message from the upper output port. When the message reaches flow-asserter out node specified by flow-asserter in node, <code>msg.payload</code> is compared with testcase's output by the rule defined in testcase and result of that comparison is sent from the lower output port.
+
+## Usage
+
+Following example flow is in examples/example.json.  
+Drag & drop in your Node-RED editor.
+
+1. Prepare the test object flow.  
+In this example, the flow consists of inject node, delay node, function node and debug node. This flow sends string, converts `msg.payload` to uppercase and display string stored in `msg.payload`.
+![intactflow](./images/intactflow.png)
+1. **Flow-asserter out node** is dragged over the mid-point of a wire which links between function node and debug node. The reason is we want to ensure the letters becoming the uppercase.
+1. Add **flow-asserter in node** to the workspace. The upper output port of flow-asserter in node is wired to the input port of delay node and the lower output port is wired to the input port of debug node which is newly added.
+![assertflow](./images/assertflow.png)
+1. Open the edit dialog of flow-asserter in node. Push add button 3 times. Fill input and output column. Second row of testcases, testID 1, will fails. Finally select flow-asserter out node's ID at the buttom of the dialog.
+![edit-dialog](./images/dialog.png)
+1. Save and close the dialog. In workspace click the button of flow-asserter in node. Test will start, node status is displayed and testcase's result is displayed in debug sidebar. Second testcase, testID 1, will be failure and the others will be success.  
+![test-finished](./images/statusfinish.png)  
+![debug-sidebar](./images/debugsidebar.png)
+
+## Install
+
+[![NPM](https://nodei.co/npm/node-red-contrib-flow-asserter.png)](https://nodei.co/npm/node-red-contrib-flow-asserter/)
+
+## Licence
+
+[Apache-2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+## Author
+
+[s1r-J](https://github.com/s1r-J)
---
+++ flow-asserter.js
@@ -0,0 +1,101 @@
+module.exports = function(RED) {
+    'use strict';
+    
+    function FlowAsserterInNode(n) {
+        
+        RED.nodes.createNode(this, n);
+        var node = this;
+        node.testcases = [];
+        for (let i in n.testcases) {
+            let tc = n.testcases[i];
+            tc.input = RED.util.evaluateNodeProperty(tc.input, tc.inputType);
+            tc.assert = RED.util.evaluateNodeProperty(tc.assert, tc.assertType);
+            tc.flowAsserterIds = {'input': n.id, 'output': n.flowasserterout};
+            node.testcases.push(tc);
+        }
+        node.testId = 0;
+        node.failedCase = 0;
+        
+        var event = 'node:' + n.id;
+        var handler = function(msg) {
+            let tc = msg._testcase;
+            let result;
+            if (tc.operator == 'eq') {
+                result = (msg.payload == tc.assert);
+            } else if (tc.operator == 'lt') {
+                result = (msg.payload < tc.assert);
+            } else { // tc.operator == 'gt'
+                result = (msg.payload < tc.assert);
+            }
+            tc.result = result ? 'Success': 'Failure';
+            node.failedCase += result ? 0 : 1;
+            node.testcases[node.testId].result = tc.result;
+            
+            node.testId++;
+            node.receive(msg);
+        }
+        RED.events.on(event, handler);
+
+        this.on('input', function(msg) {
+            this.status({
+                fill: (node.failedCase == 0 ? 'green' : 'red'),
+                shape: 'ring',
+                text: RED._('flow-asserter-in.status.running', {currentnum: (node.testId + 1), testcasenum: node.testcases.length})
+            });
+            let resultMsg = node.testId == 0 ? null: {'payload': node.testcases[node.testId - 1]};
+            if (node.testId < node.testcases.length) { // testcase remains
+                msg._testcase = node.testcases[node.testId];
+                msg.payload = msg._testcase['input'];
+                this.send([msg, resultMsg]);
+            } else { // no testcase is left
+                this.status({
+                    fill: (node.failedCase == 0 ? 'green' : 'red'),
+                    shape: 'dot',
+                    text: RED._('flow-asserter-in.status.finished', {failedcase: node.failedCase})
+                });
+                this.send([null, resultMsg])
+                node.testId = 0;
+                node.failedCase = 0;
+            }
+        });
+        this.on('close',function() {
+            this.status({});
+            RED.events.removeListener(event, handler);
+        });
+    }
+
+    RED.nodes.registerType('flow-asserter in', FlowAsserterInNode);
+
+    RED.httpAdmin.post('/flow-asserter-in/:id', RED.auth.needsPermission('flow-asserter in.write'), function(req,res) {
+        var node = RED.nodes.getNode(req.params.id);
+        if (node != null) {
+            try {
+                node.receive();
+                res.sendStatus(200);
+            } catch(err) {
+                res.sendStatus(500);
+                node.error(RED._('flow-asserter-in.notification.failed',{error:err.toString()}));
+            }
+        } else {
+            res.sendStatus(404);
+        }
+    });
+    
+    function FlowAsserterOutNode(n) {
+        
+        RED.nodes.createNode(this, n);
+        var node = this;
+        node.id = n.id;
+        
+        this.on('input', function(msg) {
+            if (msg._testcase !== undefined && msg._testcase.flowAsserterIds.output == node.id) {
+                let event = 'node:' + msg._testcase.flowAsserterIds.input;
+                RED.events.emit(event, msg);
+            } else {
+                this.send(msg);
+            }
+        });
+    }
+
+    RED.nodes.registerType('flow-asserter out', FlowAsserterOutNode);
+}
